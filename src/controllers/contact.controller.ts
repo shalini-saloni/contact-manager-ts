d83e0bb0fecb41227bef61c0abcf2cbf1d1c0ee0
Shalini Saloni <shalinisaloni2801@gmail.com>
2026-02-02 22:29:36 +0530

    updated contact controller

--- src/controllers/contact.controller.ts
+++ src/controllers/contact.controller.ts
@@ -1,25 +1,10 @@
-import { Router, Request, Response, NextFunction } from 'express';
-import { Controller } from '../utils/interfaces';
+import { Request, Response, NextFunction } from 'express';
 import ContactService from '../services/contact.service';
-import { authMiddleware } from '../middlewares/auth.middleware'; 
 
-class ContactController implements Controller {
-  public path = '/contacts';
-  public router = Router();
+class ContactController {
   private contactService = new ContactService();
 
-  constructor() {
-    this.initializeRoutes();
-  }
-
-  private initializeRoutes() {
-    this.router.post(`${this.path}`, authMiddleware, this.create);
-    this.router.get(`${this.path}`, authMiddleware, this.getAll);
-    this.router.put(`${this.path}/:id`, authMiddleware, this.update);
-    this.router.delete(`${this.path}/:id`, authMiddleware, this.delete);
-  }
-
-  private create = async (req: Request | any, res: Response, next: NextFunction) => {
+  public create = async (req: Request | any, res: Response, next: NextFunction) => {
     try {
       const result = await this.contactService.createContact(req.body, req.user.id);
       res.status(201).json(result);
@@ -28,7 +13,7 @@ class ContactController implements Controller {
     }
   };
 
-  private getAll = async (req: Request | any, res: Response, next: NextFunction) => {
+  public getAll = async (req: Request | any, res: Response, next: NextFunction) => {
     try {
       const result = await this.contactService.getAllContacts(req.user.id, req.query);
       res.status(200).json(result);
@@ -37,7 +22,7 @@ class ContactController implements Controller {
     }
   };
 
-  private update = async (req: Request | any, res: Response, next: NextFunction) => {
+  public update = async (req: Request | any, res: Response, next: NextFunction) => {
     try {
       const result = await this.contactService.updateContact(req.params.id, req.body, req.user.id);
       res.status(200).json(result);
@@ -46,7 +31,7 @@ class ContactController implements Controller {
     }
   };
 
-  private delete = async (req: Request | any, res: Response, next: NextFunction) => {
+  public delete = async (req: Request | any, res: Response, next: NextFunction) => {
     try {
       await this.contactService.deleteContact(req.params.id, req.user.id);
       res.status(200).json({ message: 'Deleted successfully' });
