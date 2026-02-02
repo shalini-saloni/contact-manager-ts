262c81a89cbecef67398fff10a935b69df5d92dc
Shalini Saloni <shalinisaloni2801@gmail.com>
2026-02-02 22:28:43 +0530

    updated auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,52 +1,24 @@
-import { Router, Request, Response, NextFunction } from 'express';
-import { Controller } from '../utils/interfaces';
+import { Request, Response, NextFunction } from 'express';
 import AuthService from '../services/auth.service';
-import { registerSchema, loginSchema } from '../dtos/auth.dto';
-import { z } from 'zod';
+import { IUser } from '../models/user.model';
 
-class AuthController implements Controller {
-  public path = '/auth';
-  public router = Router();
+class AuthController {
   private authService = new AuthService();
 
-  constructor() {
-    this.initializeRoutes();
-  }
-
-  private initializeRoutes() {
-    this.router.post(`${this.path}/register`, this.validate(registerSchema), this.signUp);
-    this.router.post(`${this.path}/login`, this.validate(loginSchema), this.logIn);
-  }
-
-  private validate = (schema: any) => (req: Request, res: Response, next: NextFunction) => {
-    try {
-      schema.parse({
-        body: req.body,
-        query: req.query,
-        params: req.params,
-      });
-      next();
-    } catch (err: any) {
-      return res.status(400).send(err.errors);
-    }
-  };
-
-  private signUp = async (req: Request, res: Response, next: NextFunction) => {
+  public signUp = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userData = req.body;
-      const signUpUserData = await this.authService.signup(userData);
+      const signUpUserData: IUser = await this.authService.signup(userData);
       res.status(201).json({ data: signUpUserData, message: 'signup' });
     } catch (error) {
       next(error);
     }
   };
 
-  private logIn = async (req: Request, res: Response, next: NextFunction) => {
+  public logIn = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const userData = req.body;
       const { token, findUser } = await this.authService.login(userData);
-      
-      res.setHeader('Authorization', `Bearer ${token}`);
       res.status(200).json({ data: findUser, token, message: 'login' });
     } catch (error) {
       next(error);
